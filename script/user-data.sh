5bd5cfb2caa1658992e12f40e3476e503ec245aa
pradhan-ashish <59129421+pradhan-ashish@users.noreply.github.com>
2020-03-10 19:50:56 -0400

    Update user-data.sh

--- script/user-data.sh
+++ script/user-data.sh
@@ -1,2 +1,20 @@
-#!/bin/bash
-echo "edt"
+vol=vol-0b1e2fe515aa43cb5
+mount_dir=/ashish
+device=/dev/xvdf/
+
+if [ ! -d $mount_dir ]
+then 
+mkdir $mount_dir 
+fi 
+
+echo "this command is to fetch instance id"
+instance_id=`curl http://169.254.169.254/latest/meta-data/instance-id`
+
+echo "this command is to attach EBS to EC2"
+aws ec2 attach-volume --volume-id $vol --instance-id $instance_id --device /dev/sdf
+
+sleep 60 
+
+echo "mount filesystem to folder" 
+
+mount $device $mount_dir
